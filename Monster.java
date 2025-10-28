a2424431704c7047dc9999b477707c5abaaa0ff4
Jayden Argudo <jayarg26@bergen.org>
2025-10-28 14:32:53 -0400

    10/28

---
+++ Monster.java
@@ -0,0 +1,57 @@
+public class Monster {
+    private int arms;
+    private int eyes;
+    private int buttons;
+    private String name;
+    public Monster(){
+
+    }
+    public Monster(int arms, int eyes, int buttons, String name){
+        this.arms = arms;
+        this.eyes = eyes;
+        this.buttons = buttons;
+        this.name = name;
+    }
+    public void setArms(int a){
+        this.arms = a;
+    }
+
+    public int getArms(){
+        return arms;
+    }
+
+    public void setEyes(int e){
+        this.eyes = e;
+    }
+
+    public int getEyes(){
+        return eyes;
+    }
+
+    public void setButtons(int b){
+        this.buttons = b;
+    }
+
+    public int getButtons(){
+        return buttons;
+    }
+
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        return "arms" + arms + "eyes" + eyes + "buttons" + buttons + "name" + name;
+    }
+
+    public static void main(String[] args){
+        Monster m1 = new Monster();
+        Monster m2 = new Monster(3, 2, 1, "Monster");
+        System.out.println(m1);  
+        System.out.println(m2);  
+    }   
+}
