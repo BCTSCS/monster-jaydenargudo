422b8a96f12ca88c1fbec1ac0921cee1913a9f6c
Jayden Argudo <jayarg26@bergen.org>
2025-11-04 14:11:19 -0500

    11/4

---
+++ SimpleClient.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class SimpleClient {
+    private Socket socket;
+    private PrintWriter out;
+    private Scanner in;
+
+    public SimpleClient(String ip, int port) throws IOException {
+        this.socket = new Socket(ip, port);
+        System.out.println("Client is connecting");
+        InputStream i = socket.getInputStream();
+        OutputStream o = socket.getOutputStream();
+        in = new Scanner(i);
+        out = new PrintWriter(o, true);
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+
+    public String receiveMessage() { 
+        return in.nextLine(); }
+
+    public void close() {}
+
+    public static void main(String[] args) throws IOException {
+        SimpleClient a = new SimpleClient("127.0.0.1", 8888);
+        a.sendMessage("hi there!");
+        String server = a.receiveMessage();
+        System.out.println("Server says: " + server);
+    }
+}
--- SimpleServer.java
+++ SimpleServer.java
@@ -9,20 +9,33 @@ public class SimpleServer {
     private Scanner in;
     public SimpleServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        System.out.println("Server started and listening "+port);
+        System.out.println("Server started on port "+port);
+        
     }
-    public void acceptClient() {
+    public void acceptClient() throws IOException {
+        // out = socket.getOutputStream();
+        // in = socket.getInputStream();
+        socket = serverSocket.accept();
+        InputStream i = socket.getInputStream();
+        OutputStream o = socket.getOutputStream();
+        in = new Scanner(i);
+        out = new PrintWriter(o, true);
     }
     public String receiveMessage() {
-        return "";
+        return in.nextLine();
     }
     public void sendMessage(String message) {
+        out.println(message);
     }
     public void close() {
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try { 
-        SimpleServer s = new SimpleServer(4888);
+        SimpleServer s = new SimpleServer(8888);
+        s.acceptClient();
+        String user = s.receiveMessage();
+        s.sendMessage("Received: "+ user);
+        s.close();
         }
         catch (Exception e){
             System.out.println("EXCEPTION OCCURED");
